77ab61b6b85264fb5159a6ee7eb10d74c70d337f
haroonbajwa <haroonbajwa99@gmail.com>
2023-07-27 18:35:36 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import DataGrid from "./components/agGrid";
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <DataGrid />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/agGrid.tsx
@@ -0,0 +1,68 @@
+"use strict";
+
+import { useMemo, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { ColDef } from "ag-grid-community";
+// import { tableData } from "../assets/data";
+import "ag-grid-enterprise";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import "../assets/agGrid.css";
+import { tableData } from "../assets/data";
+
+type DataItem = {
+  orgHierarchy: string[];
+  jobTitle: string;
+  employmentType: string;
+};
+
+const DataGrid = () => {
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [columnDefs, setColumnDefs] = useState<ColDef<DataItem>[]>([
+    // Note the use of 'ColDef<DataItem>' to specify the types for the columns
+    { field: "jobTitle" },
+    { field: "employmentType" },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+    };
+  }, []);
+  const autoGroupColumnDef = useMemo(() => {
+    return {
+      headerName: "Organisation Hierarchy",
+      minWidth: 300,
+      cellRendererParams: {
+        suppressCount: true,
+      },
+    };
+  }, []);
+  const getDataPath = useMemo(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (data: any) => {
+      return data.orgHierarchy;
+    };
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div className="example-wrapper">
+        <div style={gridStyle} className="ag-theme-alpine">
+          <AgGridReact
+            rowData={tableData}
+            columnDefs={columnDefs}
+            defaultColDef={defaultColDef}
+            autoGroupColumnDef={autoGroupColumnDef}
+            treeData={true}
+            animateRows={true}
+            groupDefaultExpanded={-1}
+            getDataPath={getDataPath}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataGrid;
