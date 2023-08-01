e6b91a1c445684c5420e6ac9be2e42241c5fb6c5
haroonbajwa <haroonbajwa99@gmail.com>
2023-08-01 10:29:08 +0500

    create nested folders, drag rows into folders

--- src/components/agGrid.tsx
+++ src/components/agGrid.tsx
@@ -21,7 +21,6 @@ type DataItem = {
 const getFileCellRenderer = () => {
   class FileCellRenderer {
     init(params: any) {
-      console.log(params, "cell renderer");
       const tempDiv = document.createElement("div");
       const value = params.value;
       const icon = "far fa-folder";
@@ -45,8 +44,8 @@ const getFileCellRenderer = () => {
 
 //component function
 const DataGrid = () => {
-  const [rowData, setRowData] = useState<DataItem[]>();
-  const [selectedItem, setSelectedItem] = useState<string>();
+  const [rowData, setRowData] = useState<DataItem[]>([]);
+  // const [selectedItem, setSelectedItem] = useState<string>();
 
   const gridRef = useRef<any>();
 
@@ -72,10 +71,11 @@ const DataGrid = () => {
       headerName: "Employees",
       minWidth: 330,
       cellRendererParams: {
-        checkbox: true,
+        // checkbox: true,
         suppressCount: true,
         innerRenderer: getFileCellRenderer(),
       },
+      rowDrag: true,
     };
   }, []);
   const getDataPath = useMemo(() => {
@@ -87,81 +87,107 @@ const DataGrid = () => {
   // add new folder under the selected folder/root folder
   const addNewGroup = useCallback(() => {
     const selectedNode = gridRef.current.api.getSelectedNodes()[0]; // Get the selected node (parent node)
+    console.log(selectedNode, "selectedNode");
 
+    // if (!selectedNode || !selectedNode.group) {
     if (!selectedNode) {
-      console.warn("No node selected. Adding to root.");
-      // If no node selected, add to the root
+      console.warn("No parent node selected. Adding to root.");
+      // If no node selected or the selected node is not a parent node, add to the root as a new group (parent) row
       const newGroupData = {
-        id: Math.floor(Math.random() * (100 - 0) + 0),
         orgHierarchy: ["New Folder"],
         jobTitle: "Folder",
         employmentType: "Folder",
+        type: "folder", // Set type to "folder" for group rows
+        children: [], // This is an empty array for children rows
       };
 
       gridRef.current.api.applyTransaction({ add: [newGroupData] });
     } else {
-      // If a node is selected, add the new folder as a child of the selected node
+      // If a parent node is selected, add the new folder as a child of the selected node
       const newGroupData = {
         id: Math.floor(Math.random() * (100 - 0) + 0),
         orgHierarchy: [...selectedNode.data.orgHierarchy, "New Folder"],
-        jobTitle: "",
-        employmentType: "",
+        jobTitle: "Folder",
+        employmentType: "Folder",
+        type: "folder", // Set type to "folder" for group rows
+        children: [], // This is an empty array for children rows
       };
 
       gridRef.current.api.applyTransaction({
         add: [newGroupData],
         addIndex: selectedNode.childIndex + 1, // Add the new folder after the selected node
       });
+
+      // Since the selected node is now a parent, update its "type" property to "folder"
+      gridRef.current.api.applyTransaction({
+        update: [{ id: selectedNode.data.id, type: "folder" }],
+      });
     }
   }, []);
 
   const isSelectionParentOfTarget = (selectedNode: any, targetNode: any) => {
-    const children = selectedNode.childrenAfterGroup || [];
-    for (let i = 0; i < children.length; i++) {
-      if (targetNode && children[i].key === targetNode.key) return true;
-      isSelectionParentOfTarget(children[i], targetNode);
+    const children = [...(selectedNode.childrenAfterGroup || [])];
+
+    if (!targetNode) {
+      return false;
     }
+
+    while (children.length) {
+      const node = children.shift();
+      if (!node) {
+        continue;
+      }
+
+      if (node.key === targetNode.key) {
+        return true;
+      }
+
+      if (node.childrenAfterGroup && node.childrenAfterGroup.length) {
+        children.push(...node.childrenAfterGroup);
+      }
+    }
+
     return false;
   };
 
-  const getRowsToUpdate = (node: any, parentPath: any) => {
-    let res: any[] = [];
-    const newPath = parentPath.concat([node.key]);
-    if (node.data) {
-      // groups without data, i.e. 'filler groups' don't need path updated
-      node.data.filePath = newPath;
-    }
-    const children = node.childrenAfterGroup || [];
-    for (let i = 0; i < children.length; i++) {
-      const updatedChildRowData = getRowsToUpdate(children[i], newPath);
-      res = res.concat(updatedChildRowData);
-    }
-    // ignore nodes that have no data, i.e. 'filler groups'
-    return node.data ? res.concat([node.data]) : res;
-  };
+  // const getRowsToUpdate = (node: any, parentPath: any) => {
+  //   let res: any[] = [];
+  //   const newPath = parentPath.concat([node.key]);
+  //   if (node.data) {
+  //     // groups without data, i.e. 'filler groups' don't need path updated
+  //     node.data.orgHierarchy = newPath;
+  //   }
+  //   const children = node.childrenAfterGroup || [];
+  //   for (let i = 0; i < children.length; i++) {
+  //     const updatedChildRowData = getRowsToUpdate(children[i], newPath);
+  //     res = res.concat(updatedChildRowData);
+  //   }
+  //   // ignore nodes that have no data, i.e. 'filler groups'
+  //   return node.data ? res.concat([node.data]) : res;
+  // };
 
-  // move selected to target node
-  const moveSelectedNodeToTarget = useCallback((targetRowId: string) => {
-    const selectedNode = gridRef.current.api.getSelectedNodes()[0]; // single selection
-    if (!selectedNode) {
-      console.warn("No nodes selected!");
-      return;
-    }
-    const targetNode = gridRef.current.api.getRowNode(targetRowId);
-    console.log(targetRowId, "target");
-    const invalidMove =
-      selectedNode?.key === targetNode?.key ||
-      isSelectionParentOfTarget(selectedNode, targetNode);
-    if (invalidMove) {
-      console.warn("Invalid selection - must not be parent or same as target!");
-      return;
-    }
-    const rowsToUpdate = getRowsToUpdate(
-      selectedNode,
-      targetNode?.data.filePath
-    );
-    gridRef.current.api.applyTransaction({ update: rowsToUpdate });
-  }, []);
+  // // move selected to target node
+  // const moveSelectedNodeToTarget = useCallback((targetRowId: string) => {
+  //   const selectedNode = gridRef.current.api.getSelectedNodes()[0]; // single selection
+  //   if (!selectedNode) {
+  //     console.warn("No nodes selected!");
+  //     return;
+  //   }
+  //   const targetNode = gridRef.current.api.getRowNode(targetRowId);
+  //   console.log(targetRowId, "target");
+  //   const invalidMove =
+  //     selectedNode?.key === targetNode?.key ||
+  //     isSelectionParentOfTarget(selectedNode, targetNode);
+  //   if (invalidMove) {
+  //     console.warn("Invalid selection - must not be parent or same as target!");
+  //     return;
+  //   }
+  //   const rowsToUpdate = getRowsToUpdate(
+  //     selectedNode,
+  //     targetNode?.data.orgHierarchy
+  //   );
+  //   gridRef.current.api.applyTransaction({ update: rowsToUpdate });
+  // }, []);
 
   const removeSelected = () => {};
 
@@ -169,8 +195,89 @@ const DataGrid = () => {
     console.log(params, "params on enter");
   };
 
-  const handleRowDragEnd = (params: any) => {
-    console.log(params, "params on end");
+  // Handle the row drag end event here
+  const handleRowDragEnd = (event: any) => {
+    // this is the row the mouse is hovering over
+    const overNode = event.overNode;
+    if (!overNode) {
+      return;
+    }
+
+    // folder to drop into is where we are going to move the file/folder to
+    const folderToDropInto =
+      overNode.data.type === "folder"
+        ? // if over a folder, we take the immediate row
+          overNode
+        : // if over a file, we take the parent row (which will be a folder)
+          overNode.parent;
+
+    // the data we want to move
+    const movingData = event.node.data;
+
+    // take new parent path from parent, if data is missing, means it's the root node,
+    // which has no data.
+    const newParentPath = folderToDropInto.data
+      ? folderToDropInto.data.orgHierarchy
+      : [];
+    const needToChangeParent = !arePathsEqual(
+      newParentPath,
+      movingData.orgHierarchy
+    );
+
+    // check we are not moving a folder into a child folder
+    const invalidMode = isSelectionParentOfTarget(event.node, folderToDropInto);
+    if (invalidMode) {
+      console.log("invalid move");
+    }
+
+    if (needToChangeParent && !invalidMode) {
+      const updatedRows: any[] = [];
+      moveToPath(newParentPath, event.node, updatedRows);
+
+      //@ts-ignore
+      gridRef.current.api.applyTransaction({
+        update: updatedRows,
+      });
+      //@ts-ignore
+      gridRef.current.api.clearFocusedCell();
+    }
+  };
+  console.log(gridRef, "gridRef");
+
+  const arePathsEqual = (path1: any, path2: any) => {
+    console.log(path1, path2, "paths");
+    if (path1.length !== path2.length) {
+      return false;
+    }
+
+    let equal = true;
+    path1.forEach(function (item: any, index: any) {
+      if (path2[index] !== item) {
+        equal = false;
+      }
+    });
+
+    return equal;
+  };
+
+  // this updates the orgHierarchy locations in our data, we update the data
+  // before we send it to AG Grid
+  const moveToPath = (newParentPath: any, node: any, allUpdatedNodes: any) => {
+    // last part of the file path is the file name
+    const oldPath = node.data.orgHierarchy;
+    const fileName = oldPath[oldPath.length - 1];
+    const newChildPath = newParentPath.slice();
+    newChildPath.push(fileName);
+
+    node.data.orgHierarchy = newChildPath;
+
+    allUpdatedNodes.push(node.data);
+
+    if (node.childrenAfterGroup) {
+      node.childrenAfterGroup.forEach((childNode: any) => {
+        moveToPath(newChildPath, childNode, allUpdatedNodes);
+      });
+    }
   };
 
   return (
@@ -180,24 +287,6 @@ const DataGrid = () => {
           <button onClick={addNewGroup} style={{ margin: "5px" }}>
             Add New Group
           </button>
-          <button
-            onClick={() =>
-              moveSelectedNodeToTarget(selectedItem?.toString() || "")
-            }
-            style={{ margin: "5px" }}
-          >
-            Move Selected to
-          </button>
-          <select onChange={(e) => setSelectedItem(e.target.value)}>
-            {rowData?.map((row) => {
-              console.log(row, "row");
-              return (
-                <option value={row.id}>
-                  {row.orgHierarchy[row.orgHierarchy.length - 1]}
-                </option>
-              );
-            })}
-          </select>
           <button onClick={removeSelected} style={{ margin: "5px" }}>
             Remove Selected
           </button>
@@ -207,12 +296,13 @@ const DataGrid = () => {
             ref={gridRef}
             rowData={rowData}
             rowDragManaged={true}
+            rowSelection="single"
             onRowDragEnter={handleRowDragEnter}
             onRowDragEnd={handleRowDragEnd}
             columnDefs={columnDefs}
             defaultColDef={defaultColDef}
             autoGroupColumnDef={autoGroupColumnDef}
-            treeData={true}
+            treeData
             animateRows={true}
             groupDefaultExpanded={-1}
             getDataPath={getDataPath}
